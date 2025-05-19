d760482073f0b3119be872178b46b20ae7214659
saidi <saidi@SAID>
2025-05-19 22:08:19 +0300

    GUI and FIx bag

--- src/main.rs
+++ src/main.rs
@@ -86,3 +86,4 @@ fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions::default();
     eframe::run_native("Tasker GUI", options, Box::new(|_cc| Box::new(app)))
 }
+//GUI version
